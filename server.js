6fc7f25c26df224c3bdc73e144fd91dad1590426
chaeeun <aug0901@naver.com>
2022-02-17 14:19:21 +0900

    AJAX로 삭제요청하기 1 (HTML 파일 구성)

--- server.js
+++ server.js
@@ -210,3 +210,16 @@ app.get('/list',function(요청,응답){
     });
 })
 
+/**
+ * 글 목록마다 삭제버튼 만들기
+ * ajax로 DELETE 요청 하면, 서버는 /delete 경로로 DELETE 요청 처리하는 코드 작성해놓으면 되겠네
+ *
+ * */
+
+// 첫줄해석 : /delete라는 경로로 DELETE요청이 왔을때 콜백함수를 실행시켜주세요
+app.delete('/delete',function(요청,응답){
+   console.log(요청.body) // 2. 요청시 함께 보낸 데이터를 찾으려면 요로케(게시물 번호)
+
+    //  3. 요청.body에 담긴 게시물 번호에 따라 DB에서 게시물 삭제
+
+});
